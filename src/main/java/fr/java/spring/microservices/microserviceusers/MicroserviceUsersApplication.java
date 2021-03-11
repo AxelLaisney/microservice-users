4a006ed692a8f91306656cc4fe0f3eb201404e65
AxelLaisney <axel.laisney@hotmail.com>
2021-03-11 16:06:33 +0100

    Project initializing commit

---
+++ src/main/java/fr/java/spring/microservices/microserviceusers/MicroserviceUsersApplication.java
@@ -0,0 +1,13 @@
+package fr.java.spring.microservices.microserviceusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/java/spring/microservices/microserviceusers/MicroserviceUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.java.spring.microservices.microserviceusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
